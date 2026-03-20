daae522369f9ab4409138d6deca8d501964a9c5f
EM-H20 <ghd0106@gmail.com>
2026-03-20 13:52:16 +0900

    feat : 게임 종료/결과 팝업 다크모드 적용 #140
    - 게임 종료 팝업 배경색/텍스트 다크모드 분기
    - 게임 결과 다이얼로그 다크모드 스타일 적용

--- lib/features/game/presentation/providers/game_event_provider.g.dart
+++ lib/features/game/presentation/providers/game_event_provider.g.dart
@@ -47,7 +47,7 @@ final gameSystemApiProvider = AutoDisposeProvider<GameSystemApi>.internal(
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef GameSystemApiRef = AutoDisposeProviderRef<GameSystemApi>;
-String _$gameEventNotifierHash() => r'4caa3d86216d179eb490b971ed14b6276dc9bd40';
+String _$gameEventNotifierHash() => r'c13f57fdeea1b56634119b7c5a7d3e3aff9112b7';
 
 /// 게임 이벤트 상태 관리 Notifier
 ///
