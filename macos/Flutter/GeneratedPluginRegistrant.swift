53e358d164e8c413988649320c31ab9b6943f651
EM-H20 <ghd0106@gmail.com>
2026-03-22 22:23:20 +0900

    feat : 설정 페이지 알림/위치 권한 기기 설정 직접 이동 적용 #154
    - app_settings 패키지 추가
    - 알림 토글 제거 → 알림 설정 화면 직접 이동으로 변경
    - 위치 권한 관리도 app_settings로 전환 (location 설정 직접 이동)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import device_info_plus
 import firebase_auth
 import firebase_core
@@ -22,6 +23,7 @@ import sign_in_with_apple
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
