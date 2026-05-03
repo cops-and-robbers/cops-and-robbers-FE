ed0f689fe0aa54ab6ffce2e6b1ecc194ba838c60
EM-H20 <ghd0106@gmail.com>
2026-05-03 21:35:14 +0900

    chore: bug feature 코드 생성 (#322)

---
+++ lib/features/bug/data/models/bug_report_request_model.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bug_report_request_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$BugReportRequestModelImpl _$$BugReportRequestModelImplFromJson(
+  Map<String, dynamic> json,
+) => _$BugReportRequestModelImpl(content: json['content'] as String);
+
+Map<String, dynamic> _$$BugReportRequestModelImplToJson(
+  _$BugReportRequestModelImpl instance,
+) => <String, dynamic>{'content': instance.content};
