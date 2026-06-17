f293ff66244d62c8112ea39bf70216ec9af9317a
EM-H20 <78837018+EM-H20@users.noreply.github.com>
2026-06-17 23:23:25 +0900

    feat : 게임 진입 위치/배터리 게이트 공용 헬퍼(GameEntryGate) 추가 #432

---
+++ lib/core/services/permission/game_entry_gate.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'game_entry_gate.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$gameEntryGateHash() => r'bb7ae1a265a656192068f01af47f157d933e7f32';
+
+/// 게임 진입 게이트 provider. 테스트에서 override 가능.
+///
+/// Copied from [gameEntryGate].
+@ProviderFor(gameEntryGate)
+final gameEntryGateProvider = Provider<GameEntryGate>.internal(
+  gameEntryGate,
+  name: r'gameEntryGateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$gameEntryGateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef GameEntryGateRef = ProviderRef<GameEntryGate>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
